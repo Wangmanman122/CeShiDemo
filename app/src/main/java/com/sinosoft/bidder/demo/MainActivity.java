598126cd3d69675bbbc371b9f43fce8adf28290e
Wangmanman01 <2326842133@qq.com>
2018-02-09 19:54:19 +0800

    qweqweqewq

---
+++ app/src/main/java/com/sinosoft/bidder/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sinosoft.bidder.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String str = "123456";
+//        byte[] decode = Base64.decode(str,0);
+//        System.out.println(decode.toString());
+        String encode = Base64.encodeToString(str.getBytes(), 0);
+        System.out.println("加密："+encode);
+    }
+}
---
+++ app/src/main/java/com/sinosoft/bidder/demo/Test.java
@@ -0,0 +1,15 @@
+package com.sinosoft.bidder.demo;
+
+/**
+ * Created by Dell on 2018/1/20.
+ */
+
+public class Test {
+    public  static  void main(String[] args){
+        String str = "123456";
+//        byte[] decode = Base64.decode(str,0);
+//        System.out.println(decode.toString());
+        String encode = Base64.encodeToString(str.getBytes(), 0);
+        System.out.println("加密："+encode);
+    }
+}
