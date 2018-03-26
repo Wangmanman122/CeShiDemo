ff49709f3c28ec1889eb98ce43a30152de94f82d
Wangmanman01 <2326842133@qq.com>
2018-03-26 16:17:04 +0800

    qweqweqewq

--- app/src/main/java/com/sinosoft/bidder/demo/MainActivity.java
+++ app/src/main/java/com/sinosoft/bidder/demo/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 //        System.out.println(decode.toString());
         String encode = Base64.encodeToString(str.getBytes(), 0);
         System.out.println("加密："+encode);
-        System.out.println("周旋男朋友");
+        
+        System.out.println("11111111111");
     }
 }
